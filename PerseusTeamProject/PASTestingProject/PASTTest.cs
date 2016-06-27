b066315f16d803a60c5a99a088708edab0d194ec
MyamotoMusashi <Knifed313@gmail.com>
2016-06-27 23:13:01 +0300

    EMO's first commit, yeii

--- PerseusTeamProject/PASTestingProject/PASTTest.cs
+++ PerseusTeamProject/PASTestingProject/PASTTest.cs
@@ -11,6 +11,7 @@
         static void Main(string[] args)
         {
             var rakiq = new PermanentExpense("Rakiq", 5.0M, ExpenseType.Food);
+            var bira = new PermanentExpense("Bira", 2.25M, ExpenseType.Food);
 
             Console.WriteLine(rakiq);
         }
